963ab82e47b417767538a41078b5ae53b530b67a
João Bomfim Povinha <84041606+joaopovinha@users.noreply.github.com>
2021-05-14 23:43:13 -0300

    Servlet Exercise Commit
    Exercício simples utilizando Java, Maven, Tomcat Server e Java Servlet API para exibir um Hello World. Realizado através do Eclipse.

---
+++ Servlet_01/src/main/java/uscs/Servlet_01.java
@@ -0,0 +1,48 @@
+package uscs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet_01
+ */
+@WebServlet("/Servlet_01")
+public class Servlet_01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet_01() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<body><h1>Servlet 01 - Hello World...</h1></body>");
+		out.print("</html>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
